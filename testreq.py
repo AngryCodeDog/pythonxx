d726cea22f6912b8476f4839341a3f2a74091c9d
zyp <zyp@zyptekiiMac.local>
2018-05-23 18:33:55 +0800

    优化代码，调整结构，绘制直线动画

--- testreq.py
+++ testreq.py
@@ -1,6 +1,8 @@
 #! encoding=utf-8
 import requests
-import json,time,datetime
+import json
+import time
+import datetime
 from logger import logger
 from reqbox.utils import requtil
 
@@ -19,13 +21,14 @@ headers = {
     "cookie": "session=409601e0-0028-4bc8-bb77-299212af41ab",
 }
 
+
 def req_recognize():
     try:
         url = 'http://www.ztdface.com/recognize'
-        file = open('twoperson2.jpg','rb')
+        file = open('twoperson2.jpg', 'rb')
         file = {'image': (file.name.split('/')[-1], file)}
         data = requests.post(url, files=file)
-        print json.loads(data.content,encoding='utf-8')
+        print json.loads(data.content, encoding='utf-8')
         writefiel(data.content)
     except Exception as e:
         logger.exception(e)
@@ -34,21 +37,22 @@ def req_recognize():
 def req_compare():
     try:
         url = 'http://192.168.1.50:8080/compare'
-        file1 = open('zrg1.jpg','rb')
-        file2 = open('zrg2.jpg','rb')
+        file1 = open('zrg1.jpg', 'rb')
+        file2 = open('zrg2.jpg', 'rb')
         files = {'image_1': (file1.name.split('/')[-1], file1),
                  'image_2': (file2.name.split('/')[-1], file2)}
         data = requests.post(url, files=files)
-        print json.loads(data.content,encoding='utf-8')
+        print json.loads(data.content, encoding='utf-8')
     except Exception as e:
         logger.exception(e)
 
 
 def writefiel(json_str):
-    str = json.dumps(json.loads(json_str,encoding='utf-8'),ensure_ascii=False)
-    f = open('./test.json','w')
+    # str = json.dumps(json.loads(json_str, encoding='utf-8'), ensure_ascii=False)
+    f = open('./test.json', 'w')
     f.write(json_str)
     f.close()
+
 
 def req_subject_list():
     """"""
@@ -58,8 +62,8 @@ def req_subject_list():
         # params = {'name':req_data.get('name',''),'title':req_data.get('title',''),
         # 'department':req_data.get('company',''),'phone':req_data.get('phone',''),
         # 'gender':req_data.get('gender',0),'remark':req_data.get('course',''),'photo_ids':req_data.get('photo_ids',[])}
-        params = {"category":"employee","size":10,"_":int(time.time())}
-        data = requests.get(url,params=params,headers=headers).json()
+        params = {"category": "employee", "size": 10, "_": int(time.time())}
+        data = requests.get(url, params=params, headers=headers).json()
         print data
         if data['code'] == 0:
             return data
@@ -69,37 +73,40 @@ def req_subject_list():
         logger.exception(e)
     return ''
 
+
 def del_subject_test():
     try:
         url = BASE_URL + '/operation/subject'+'/139'
         proxies = {'http': None, 'https': None}
-        data = requests.delete(url,proxies=proxies)
-        
-        print json.loads(data.content,encoding='utf-8')
+        data = requests.delete(url, proxies=proxies)
+
+        print json.loads(data.content, encoding='utf-8')
     except Exception as e:
         logger.exception(e)
+
 
 def update_subject():
     try:
         url = BASE_URL + '/operation/subject/140'
         # file = {'image':('filename.jpg',open('zyp2.jpg','rb'))}
-        params = {"name":"朱宇培","company":"阿里","remark":"演讲课"}
+        params = {"name": "朱宇培", "company": "阿里", "remark": "演讲课"}
         proxies = {'http': None, 'https': None}
-        data = requests.put(url, json=params,proxies=proxies)
-        
+        data = requests.put(url, json=params, proxies=proxies)
+
         print data.content
     except Exception as e:
         logger.exception(e)
 
-def req_update_subject(subject_id,req_data):
+
+def req_update_subject(subject_id, req_data):
     """请求更新个人信息"""
     try:
         url = BASE_URL + '/subject/'+str(subject_id)
-        params = {'name':req_data.get('name',''),'title':req_data.get('title',''),
-        'department':req_data.get('company',''),'phone':req_data.get('phone',''),
-        'gender':req_data.get('gender',0),'remark':req_data.get('course',''),'photo_ids':req_data.get('photo_ids',[])}
+        params = {'name': req_data.get('name', ''), 'title': req_data.get('title', ''),
+                  'department': req_data.get('company', ''), 'phone': req_data.get('phone', ''),
+                  'gender': req_data.get('gender', 0), 'remark': req_data.get('course', ''), 'photo_ids': req_data.get('photo_ids', [])}
         logger.info(json.dumps(params))
-        data = requests.put(url,json=params,headers=headers).json()
+        data = requests.put(url, json=params, headers=headers).json()
         print data
         return data
     except Exception as e:
@@ -108,9 +115,9 @@ def req_update_subject(subject_id,req_data):
 
 def req_update_photo():
     url = BASE_URL + '/subject/photo'
-    params = {"subject_id":149,"photo_id":186}
-    file = {"image":("filename.jpg",open('zyp2.jpg','rb'))}
-    data = requests.post(url,data=params,files=file,headers=headers).json()
+    params = {"subject_id": 149, "photo_id": 186}
+    file = {"image": ("filename.jpg", open('zyp2.jpg', 'rb'))}
+    data = requests.post(url, data=params, files=file, headers=headers).json()
     print data
     return data
 
@@ -119,18 +126,18 @@ def req_sync_event():
     image = requtil.jpg_to_base64str('zyp2.jpg')
 
     event_data = {
-            'screen_token': '3cd9332a-d220-413c-b325-0c199f36cc9a',
-            'photo': image,
-            'age': 24,
-            'gender': 1,
-            'group': -1,
-            'short_group': -1,
-            'quality': 0.975,
-            'confidence': 89.99987,
-            'event_type': 0,
-            'subject_id':None
-        }
-    data = requests.post('http://192.168.1.50/sync/event',data=event_data)
+        'screen_token': '3cd9332a-d220-413c-b325-0c199f36cc9a',
+        'photo': image,
+        'age': 24,
+        'gender': 1,
+        'group': -1,
+        'short_group': -1,
+        'quality': 0.975,
+        'confidence': 89.99987,
+        'event_type': 0,
+        'subject_id': None
+    }
+    data = requests.post('http://192.168.1.50/sync/event', data=event_data)
     print data.content
 
 
@@ -148,16 +155,38 @@ def req_records():
     - date 
     """
     url = 'http://192.168.1.50/attendance/records/monthly'
-    today = datetime.date.today()  
+    today = datetime.date.today()
     today_str = today.strftime('%Y%m')
-    params = {'subject_id':58,'date':today_str,'_':int(time.time())}
-    data = requests.get(url,params=params,headers=headers)
+    params = {'subject_id': 58, 'date': today_str, '_': int(time.time())}
+    data = requests.get(url, params=params, headers=headers)
     print data.content
     result = json.loads(data.content)
-    today_num = int(today.strftime('%d')) -1
+    today_num = int(today.strftime('%d')) - 1
     print result['data']['records'][today_num]
 
 
+def testdlib():
+    import cv2
+    import dlib
+    detector = dlib.get_frontal_face_detector()
+    landmark_predictor = dlib.shape_predictor('/Users/zyp/Downloads/shape_predictor_68_face_landmarks.dat')
+    img = cv2.imread('/Users/zyp/workspace/pythontest/twoperson.jpg')
+    faces = detector(img, 1)
+    if (len(faces) > 0):
+        for k, d in enumerate(faces):
+            print type(d)
+            print faces
+            cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (255, 255, 255))
+            td = dlib.rectangle(d.left(), d.top(), d.right(), d.bottom())
+            print td
+            shape = landmark_predictor(img, td)
+            print shape
+            for i in range(68):
+                cv2.circle(img, (shape.part(i).x, shape.part(i).y), 2, (0, 255, 0), -1, 8)
+                # cv2.putText(img,str(i),(shape.part(i).x,shape.part(i).y),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,2555,255))
+    cv2.imshow('Frame', img)
+    cv2.waitKey(0)
+
+
 if __name__ == "__main__":
-    requtil.login()
-    requtil.req_subject_info(58)
+    testdlib()
--- testthreading.py
+++ testthreading.py
@@ -2,7 +2,7 @@
 
 import threading
 import json
-import time
+#  time
 import Tkinter as tk
 import cv2
 from PIL import Image, ImageTk
@@ -11,27 +11,39 @@ from Queue import Queue
 from StringIO import StringIO
 import requests
 import threadpool
-import imutils
+# import imutils
 import tkFont
+import dlib
 
-
+# opencv脸部检测默认数据集
 face_patterns = cv2.CascadeClassifier('/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+# dlib脸部检测
+detector = dlib.get_frontal_face_detector()
+# dlib脸部68个特征点检测数据集
+landmark_predictor = dlib.shape_predictor('/Users/zyp/Downloads/shape_predictor_68_face_landmarks.dat')
 
 SHOW_RECOGNIZE_WINDOW = 101
 SHWO_STRANGER_WINDOW = 102
 CLOSE_RECOGNIZE_WINDOW = 100
 
+# 控制显示矩形框内直线运动
+cur_line_y = 8
+
 
 def thread_recoginze(frame):
+    """ 请求识别处理 """
+    # 把opencv的数据转成图片二进制流发送给服务器
     ndarray_convert_img = Image.fromarray(frame)
     image_buf = save_to_jpeg(ndarray_convert_img)
     data = recognize(image_buf.getvalue())
     logger.info(data)
+    # 对图片做大小处理
     img_resize = ndarray_convert_img.resize((300, 300))
+    # 转成可以供tk.label显示的图片对象
     imagetk = ImageTk.PhotoImage(img_resize)
+    # 放入消息到消息队列，供主线程去切换UI
     msg = Message()
-    if data['code'] == 0 and data['data'].get('name', None) != None:
-        # pba.second_windows.show_window(data['data'], imagetk)
+    if data['code'] == 0 and data['data'].get('name', None) is not None:
         msg = Message(SHOW_RECOGNIZE_WINDOW, {'data': data['data']})
     else:
         msg = Message(SHWO_STRANGER_WINDOW, {'data': {}})
@@ -39,14 +51,17 @@ def thread_recoginze(frame):
     msg.img = imagetk
     my_message_queue.put_msg(msg)
 
-    logger.info(threading.current_thread().name + 'complete')
-
 
 def recognize(img_byte):
     url = 'http://www.ztdface.com/recognize'
     ret = None
     try:
-        ret = requests.post(url, {'group': 'http://127.0.0.1:8866/sync/features', "quality": ""}, files={'image': ('filename.jpg', img_byte)})
+        ret = requests.post(
+            url, {
+                'group': 'http://127.0.0.1:8866/sync/features',
+                "quality": ""
+            },
+            files={'image': ('filename.jpg', img_byte)})
         return json.loads(ret.content)
     except Exception as e:
         print e
@@ -54,6 +69,7 @@ def recognize(img_byte):
 
 
 def save_to_jpeg(im):
+    """ 把图片转成二进制流 """
     buf = StringIO()
     try:
         im.save(buf, format='jpeg')
@@ -62,20 +78,54 @@ def save_to_jpeg(im):
     return buf
 
 
+def get_face_point(ndarray_rgb):
+    """ 检测人脸并绘制脸部矩形框和特征点 """
+    # 转成灰度图片，更易识别人脸
+    gray = cv2.cvtColor(ndarray_rgb, cv2.COLOR_RGB2GRAY)
+    faces = face_patterns.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3, minSize=(100, 100))
+    detect = False  # 是否检测到人脸
+    face_img_list = []  # 人脸数据数据
+    for (x, y, w, h) in faces:
+        # 绘制脸部矩形
+        cv2.rectangle(ndarray_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # 截图保存去请求识别(numpy里面要保存切片的副本，不然会受后面代码的影响)
+        face_img_list.append(ndarray_rgb[y: y+h, x: x+w].copy())
+        # 在脸部框里面绘制动画线条
+        ndarray_rgb = draw_line_in_rect(ndarray_rgb, x, y, x + w, y + h)
+        # 绘制人脸特征点,减少计算次数，2帧描绘一次
+        if cur_line_y % 16 == 0:
+            shape = landmark_predictor(ndarray_rgb, dlib.rectangle(x, y, x + w, y + h))
+            for i in range(68):
+                cv2.circle(ndarray_rgb, (shape.part(i).x, shape.part(i).y), 1, (0, 255, 0), -1, 8)
+        detect = True
+
+    result = {"detect": detect, "data": face_img_list, "frame": ndarray_rgb}
+    return result
+
+
+def draw_line_in_rect(img_ndrray, x1, y1, x2, y2):
+    """ 在矩形框内画滚动直线动画 """
+    global cur_line_y
+    if y1 + cur_line_y > y1 and y1 + cur_line_y < y2:
+        # 画线
+        cv2.line(img_ndrray, (x1 + 1, y1 + cur_line_y), (x2 - 1, y1 + cur_line_y), (0, 255, 0), 4)
+        cur_line_y += 8
+    else:
+        cur_line_y = 8
+    return img_ndrray
+
+
 def get_face(ndarray_img):
-    """ 检测人脸并绘制脸部矩形框 """
-    # BGR转RGB恢复正常色调，不然有点显蓝色
     ndarray_rgb = cv2.cvtColor(ndarray_img, cv2.COLOR_BGR2RGB)
     # 转成灰度图片，更易识别人脸
     gray = cv2.cvtColor(ndarray_img, cv2.COLOR_BGR2GRAY)
     faces = face_patterns.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3, minSize=(100, 100))
-    detect = False
     face_img_list = []
+    detect = False
     for (x, y, w, h) in faces:
         cv2.rectangle(ndarray_rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        face_img_list.append(ndarray_rgb[y: y + h, x: x + w])
+        face_img_list.append(ndarray_rgb[y:y + h, x:x + w])
         detect = True
-
     result = {"detect": detect, "data": face_img_list, "frame": ndarray_rgb}
     return result
 
@@ -88,11 +138,12 @@ class PhotoBoothApp:
         self.thread = None
         self.stopEvent = None
 
+        # 创建根窗口
         self.root = tk.Tk()
-        parent_w, parent_h = self.root.maxsize()
+        parent_w, parent_h = self.root.maxsize()  # 获取屏幕大小
         locationstr = '%dx%d+%d+%d' % (1200, 800, (parent_w - 1200) / 2, (parent_h - 800) / 2)
         self.root.geometry(locationstr)
-
+        # 设置视频帧label
         self.panel = tk.Label(self.root)
         self.panel.pack(side="top", padx=10, pady=10)
 
@@ -104,7 +155,7 @@ class PhotoBoothApp:
         self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
         self.cur_frame_num = 1
-        self.timeF = 20
+        self.timeF = 20  # 隔多少帧去做人脸识别
 
         self.videoLoop()
 
@@ -112,22 +163,25 @@ class PhotoBoothApp:
         try:
             if not self.stopEvent.is_set():
                 tt, self.frame = self.vs.read()
+                # BGR转RGB恢复正常色调，不然视频画面偏蓝色
+                self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
                 # self.frame = imutils.resize(self.frame, width=2400)
-                detect_data = get_face(self.frame)
+                detect_data = get_face_point(self.frame)
                 self.frame = detect_data['frame']
                 if self.cur_frame_num % self.timeF == 0 and detect_data['detect']:
                     reqs = threadpool.makeRequests(thread_recoginze, detect_data['data'])
                     [pool.putRequest(req) for req in reqs]
                 self.cur_frame_num += 1
-
+                # 更换帧图片
                 self.change_image()
+                # 读取消息队列
                 my_message_queue.loop_queue()
-                self.panel.after(50, self.videoLoop)
+                self.panel.after(100, self.videoLoop)
         except RuntimeError, e:
+            print e
             print("[INFO] caught a RuntimeError")
 
     def change_image(self):
-        # ndarray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
         ndarray_2_image = Image.fromarray(self.frame)
         imagetk = ImageTk.PhotoImage(ndarray_2_image)
         if self.panel is None:
@@ -173,7 +227,7 @@ class SecondWindow(object):
         ft = tkFont.Font(size=60, weight=tkFont.BOLD)
         label = tk.Label(top, text=text, font=ft, fg='red')
         label.pack()
-        # 加入字典中
+        # toplevel加入字典中
         self.windows_dict[self.current_toplevel] = top
         # 设置top窗口位置
         srcw, srch = top.maxsize()
@@ -185,19 +239,11 @@ class SecondWindow(object):
         # 3s后去自动关闭该弹窗
         top.after(3000, self.close_top, self.current_toplevel)
 
-    # def close_top(self, top_key):
-    #     time.sleep(3)
-    #     logger.info(threading.current_thread().name + '--prepare to closeing window')
-    #     msg = Message(CLOSE_RECOGNIZE_WINDOW, {'key': top_key})
-    #     my_message_queue.put_msg(msg)
-
     def close_top(self, top_key):
         try:
-            # logger.info(threading.current_thread().name + '--close current toplevel--' + 'top_key=' + top_key + '--' + str(self.windows_dict))
             if top_key in self.windows_dict.keys():  # key是否存在
                 self.windows_dict[top_key].destroy()
                 self.windows_dict.pop(top_key)
-                # logger.info(threading.current_thread().name + '--close current toplevel completed!!')
         except Exception as e:
             print e
 
@@ -248,7 +294,6 @@ class HandlerMsg(object):
 
     def handle_message(self, msg):
         # 处理线程的消息，如创建弹窗，消失弹窗
-        # logger.info(threading.current_thread().name + '--msg.what = ' + str(msg.what))
         cur_key = ''
         args = ()
         imagetk = msg.img
@@ -275,11 +320,9 @@ class HandlerMsg(object):
 my_handler = HandlerMsg()
 my_message_queue = MessageQueue()
 
-
 pool = threadpool.ThreadPool(10)
 pba = PhotoBoothApp()
 second_windows = SecondWindow()
-
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
